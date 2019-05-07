477abdf9332395edea00c12c8e959f9be70c3079
Shahid1993 <31109285+Shahid1993@users.noreply.github.com>
2019-05-07 11:09:44 +0530

    Create 001_watchDirectory.sh

---
+++ 001_watchDirectory.sh
@@ -0,0 +1,33 @@
+shopt -s extglob
+inotifywait -m -r /home/codemantra/Documents/OAT/RD/PDFixSDKSample-java-master/sourcepdf -e close_write -e moved_to |
+    while read path action file; do
+        cd /home/codemantra/Documents/OAT/RD/PDFixSDKSample-java-master/
+        echo "The file '$file' appeared in directory '$path' via '$action'"
+        filename=$(basename -- "$file")
+        name="${filename%.*}"
+        extension="${filename##*.}"
+
+        echo "$filename"
+        
+	if [[ -d $path/$file ]]; then
+	    echo "$file is a directory";
+    	else
+	    echo "$file is a file";
+	    if [ "$extension" == "pdf" ]; then
+	    	echo "file is pdf...";
+		inputPath="$(cd "$(dirname "$path")"; pwd)/$(basename "$path")";
+		outputPath=${inputPath/\/sourcepdf/\/inputpdf};
+
+		mkdir -p "$outputPath"
+
+		pdfix_app shahid@codemantra.in gaXz94Eomk512fev -pdf2xml "$inputPath"/"$filename" "$outputPath"/"$name".xml
+
+		pdfix_app shahid@codemantra.in gaXz94Eomk512fev -addtags "$inputPath"/"$filename" "$outputPath"/"$filename"
+
+		#shopt -s extglob
+		cd "$outputPath"/
+		rm -v !(*.xml|*.pdf)		
+	    fi 
+        fi
+        echo "---------------";
+    done
